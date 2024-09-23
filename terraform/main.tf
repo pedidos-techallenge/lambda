37e98cc5a0385c8d29d4d6971f9ada3970199413
Ingrid Queiroz <queirozingrid.c@gmail.com>
2024-09-23 17:17:46 -0300

    Change application resource path

--- terraform/main.tf
+++ terraform/main.tf
@@ -63,7 +63,7 @@ data "aws_api_gateway_rest_api" "api" {
 resource "aws_api_gateway_resource" "application_resource" {
   rest_api_id = data.aws_api_gateway_rest_api.api.id
   parent_id   = data.aws_api_gateway_rest_api.api.root_resource_id
-  path_part        = "pedidosapi"
+  path_part        = "application"
 }
 
 resource "aws_api_gateway_method" "get_method" {
