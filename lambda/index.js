caca21a26e5e4ae539147277860fdfcfb3b9f927
Ingrid Queiroz <queirozingrid.c@gmail.com>
2024-09-23 22:38:43 -0300

    Lambda redirects to cognito

--- lambda/index.js
+++ lambda/index.js
@@ -1,30 +1,47 @@
-const fs = require('fs');
-const path = require('path');
+const AWS = require('aws-sdk');
+
+const clientId = process.secrets.CLIENT_ID;
+const cognitoDomain = process.secrets.COGNITO_DOMAIN;
+const redirectUri = process.secrets.REDIRECT_URI;
 
 exports.handler = async (event) => {
-  // Caminho para o arquivo HTML
-  const filePath = path.join(__dirname, 'index.html');
+    const requestBody = JSON.parse(event.body);
+    const cognitoUrl = `https://${cognitoDomain}.auth.us-east-1.amazoncognito.com/login?client_id=${clientId}&response_type=code&scope=openid&redirect_uri=${redirectUri}`;
 
-  // Leitura do arquivo HTML
-  let htmlContent;
-  try {
-    htmlContent = fs.readFileSync(filePath, 'utf8');
-  } catch (err) {
-    console.error('Erro ao ler o arquivo HTML:', err);
-    return {
-      statusCode: 500,
-      body: 'Erro interno ao carregar a página.',
-    };
-  }
 
-  // Resposta HTTP com o conteúdo do HTML
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'text/html',
-    },
-    body: htmlContent,
-  };
+    // Verify if CPF is in the body
+    const cpf = requestBody.cpf;
 
-  return response;
+    if (cpf) {
+        try {
+            return {
+                statusCode: 302,
+                headers: {
+                    Location: cognitoUrl,
+                },
+                body: JSON.stringify({
+                    message: 'Redirecting to cognito authentication...',
+                }),
+            };
+        } catch (error) {
+            return {
+                statusCode: 500,
+                body: JSON.stringify({
+                    message: 'Failed to conect to cognito.',
+                    error: error.message,
+                }),
+            };
+        }
+    } else {
+        // Se não houver CPF, redireciona diretamente para a aplicação
+        return {
+            statusCode: 302,
+            headers: {
+                Location: 'https://pudim.com', // URL da aplicação
+            },
+            body: JSON.stringify({
+                message: 'Redirecting to application...',
+            }),
+        };
+    }
 };
