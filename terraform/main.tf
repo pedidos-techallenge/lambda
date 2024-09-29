caa71da0ab33e099427c25de05af5f0305d1a231
Ingrid Queiroz <queirozingrid.c@gmail.com>
2024-09-28 21:30:25 -0300

    Add lambda integration cpf on deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -131,7 +131,7 @@ resource "aws_lambda_permission" "api_gateway_permission" {
 
 # Deploy API Gateway
 resource "aws_api_gateway_deployment" "api_deployment" {
-  depends_on   = [aws_api_gateway_integration.lambda_integration, aws_lambda_permission.api_gateway_permission]
+  depends_on   = [aws_api_gateway_integration.lambda_integration, aws_lambda_permission.api_gateway_permission, aws_api_gateway_integration.lambda_integration_cpf]
   rest_api_id  = data.aws_api_gateway_rest_api.api.id
   stage_name   = "prod"
 }
